4fa60661d18e1b51e7dbbe88f547ed050c40fb70
Giang Bui <bhuonggiang03@gmail.com>
2022-01-18 14:35:59 -0500

    add types & env

--- libs/shared-types/src/lib/shared-types.ts
+++ libs/shared-types/src/lib/shared-types.ts
@@ -1,3 +1,52 @@
-export function sharedTypes(): string {
-  return 'shared-types';
+export enum GameTypes {
+  TRUTHORDARE,
+  CARDDRAWING,
+  HAVEYOUEVER,
+  WOULDYOURATHER,
+  MOODGUESS,
+}
+
+export type Metadata = {
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export interface Player {
+  // id: string;
+  name: string;
+  session: Session;
+  // answers: Answer[];
+}
+
+export interface Session {
+  id: string;
+  host: Player;
+  players: Player[];
+  games: Game[];
+  metadata: Metadata
+}
+
+export interface Game {
+  // id: string;
+  type: GameTypes;
+  timer?: Date;
+  // sessions: Session[];
+  questions: Question[];
+  metadata: Metadata;
+}
+
+export interface Question {
+  // id: string;
+  // creator: Player;
+  // game: Game;
+  content: string;
+  // answers?: Answer[];
+  metadata: Metadata;
+}
+
+export interface Answer {
+  // id: string;
+  owner: Player;
+  content: string;
+  // question: Question;
 }
